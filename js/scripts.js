92030320d4fd056f201d52f8f94b38701f57d729
Jacek Jarczok <kson.eu@gmail.com>
2020-05-25 14:33:08 +0200

    Develop section 03 and 04

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,54 @@ const headerRoborockImages = document.querySelectorAll('.s6MaxV__01-header__img'
 const headerRoborockImage = document.querySelector('.s6MaxV__01-header__img');
 let headerRoborockImageTop = headerRoborockImage.offsetTop;
 
-// Helper functions
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
+// 03 Video and text
+const section03 = document.querySelector('.s6MaxV__03-video');
+const textSection03 = document.querySelector('.s6MaxV__03-video__text');
+const videoSection03 = document.querySelector('.s6MaxV__03-video__video');
+let section03OffsetTop;
 
-//// 01 HEADER
+
+// Helper functions
+function debounced(delay, fn) {
+  let timerId;
+  return function (...args) {
+      if (timerId) {
+          clearTimeout(timerId);
+      }
+      timerId = setTimeout(() => {
+          fn(...args);
+          timerId = null;
+      }, delay);
+  }
+}
+
+function throttled(delay, fn) {
+  let lastCall = 0;
+  return function (...args) {
+      const now = (new Date).getTime();
+      if (now - lastCall < delay) {
+          return;
+      }
+      lastCall = now;
+      return fn(...args);
+  }
+}
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+//// *** 01 HEADER ***
 // update headerRoborockImageTop value on window resize
 window.addEventListener('resize', () => headerRoborockImageTop = headerRoborockImage.offsetTop);
 
@@ -46,3 +77,28 @@ function replaceHeaderRoborockImages() {
 
 window.addEventListener('scroll', fixedPositionHeaderRoborockImages);
 window.addEventListener('scroll', replaceHeaderRoborockImages);
+//// ** END OF: 01 HEADER **
+
+
+//// *** 03 VIDEO AND TEXT ***
+// when 'section03' comes into view, slide it up and read it offsetTop
+const slideUpSection03 = function() {
+  if (isElementInViewport(section03)) {
+    section03.classList.add('slideUp03ToTop');
+    section03.addEventListener('transitionend', () => {
+      textSection03.classList.add('slideUp03ToTop');
+      videoSection03.play();
+    });
+    section03OffsetTop = section03.offsetTop;
+  }
+};
+
+// hide headerRoborockImages when pageYOffset value is higher than section03 offsetTop
+const hideHeaderRoborockImages = function() {
+  if (window.pageYOffset > section03OffsetTop) {
+    headerRoborockImages.forEach(el => el.classList.add('displayNone'));
+  }
+};
+
+window.addEventListener('scroll', slideUpSection03);
+window.addEventListener('scroll', hideHeaderRoborockImages);
